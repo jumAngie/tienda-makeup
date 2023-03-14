67cd9717e40f5f6f2f2f1156589a65dd4c1343c6
Angie <125337384+jumAngie@users.noreply.github.com>
2023-03-14 11:29:58 -0600

    ScripsDataBase de Crear

--- Maquillaje/Maquillaje.DataAccess/Repositories/ScriptsDataBase.cs
+++ Maquillaje/Maquillaje.DataAccess/Repositories/ScriptsDataBase.cs
@@ -7,23 +7,51 @@ namespace Maquillaje.DataAccess.Repositories
     public class ScriptsDataBase
     {
         #region Categorias
-        public static string CategoriasList = "UDP_Maqui_tbCategorias_VISTA";
+        public static string CategoriasCrear = "UDP_Gral_tbClientes_CREAR";
         #endregion
 
         #region Productos
-
+        public static string ProductosCrear = "UDP_Maqui_tbProductos_CREAR";
         #endregion
 
         #region Empleados
-
+        public static string EmpleadosCrear = "UDP_Gral_tbEmpleados_CREAR";
         #endregion
 
         #region Clientes
-
+        public static string ClientesCrear = "UDP_Gral_tbClientes_CREAR";
         #endregion
 
         #region Sucursales
+        public static string SucursalesCrear = "UDP_Gral_tbSucursales_CREAR";
+        #endregion
 
+        #region Departamentos
+        public static string DepartamentosCrear = "UDP_Gral_tbSucursales_CREAR";
+        #endregion
+
+        #region Municipios
+        public static string MunicipiosCrear = "UDP_Gral_tbMunicipios_CREAR";
+        #endregion
+
+        #region Usuarios
+        public static string UsuariosCrear = "UDP_Gral_tbUsuarios_CREAR";
+        #endregion
+
+        #region MetodoPago
+        public static string MetodoPagoCrear = "UDP_Maqui_tbMetodoPago_CREAR";
+        #endregion
+
+        #region Proveedores
+        public static string ProveedoresCrear = "UDP_Maqui_tbProveedores_CREAR";
+        #endregion
+
+        #region Ventas
+        public static string VentasCrear = "UDP_Maqui_tbVentas_CREAR";
+        #endregion
+
+        #region VentasDetalles
+        public static string VentasDetallesCrear = "UDP_Maqui_tbVentasDetalle_CREAR";
         #endregion
     }
 }
